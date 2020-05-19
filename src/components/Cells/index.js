47bf4b68754e6183087e8f9ed8ba95b9d42663be
ferraripuppi <habin@hotmail.co.uk>
2020-05-20 00:29:30 +0100

    sets up server.js + heroku

--- src/components/Cells/index.js
+++ src/components/Cells/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import * as S from './styles';
-import { func, object, instanceOf } from 'prop-types';
+import { object, instanceOf } from 'prop-types';
 
 const Cells = ({ dateFns, currentDate, selectedDate }) => {
   const monthStart = dateFns.startOfMonth(currentDate);
